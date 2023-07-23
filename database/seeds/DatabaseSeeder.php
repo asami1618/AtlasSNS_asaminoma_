c2c2c82bdae043a2a92dd6556a99dbd355875cfd
野間亜沙美 <>
2023-07-23 17:22:41 +0900

    SNS課題再コミット

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //  7/9 下記コード追記
+        $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/FollowsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FollowsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            ['following_id' => 'following'],
+            ['followed_id' => 'followed'],
+        ]);
+
+    }
+}
