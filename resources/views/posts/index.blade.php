312fd55ace12cb6346143e5db7d9c9dfb99c45ee
野間亜沙美 <>
2023-09-04 21:57:45 +0900

    9/4 コミット1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h2>機能を実装していきましょう。</h2>
-    {!! Form::open(['url' => '/posts/index']) !!}
+    {!! Form::open(['url' => '/added']) !!}
     {{Form::token()}}
     <div class="form-group">
     {!! Form::input('text','newPost', null, ['required','class' => 'form-control','plaseholder' => '投稿内容' ]) !!}
