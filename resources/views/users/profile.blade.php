515e89fdd9fa8bf68c764e4be3bbac0ed2c71c70
野間亜沙美 <>
2023-12-02 17:06:13 +0900

    12/2 コミット2

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -17,10 +17,35 @@
         </div>
             <div class="othersProfile">
                 <p>{{ $post->user->username }}</p>
-                <p>{{ $post->bio }}</p>
+                <p><textarea name="introduction"></textarea></p>
             </div>
         @endforeach
-        @endif            
+        @endif
+
+        <div>
+            @if (auth()->user()->isFollowing($users->id))
+            <!-- フォロー解除 -->
+                <a href="{{ route('unfollow' , $users->id) }}" class="btn unfollow_btn">フォロー解除</a>
+            @else
+            <!-- フォローする -->
+                <a href="{{ route('follow' , $users->id) }}" class="btn follow_btn">フォローする</a>
+            @endif
+        </div>
+
+        <div>
+            <h2> [ 投稿一覧 ] </h2>
+                <table>
+                    @foreach($posts as $post)
+                    <tr>
+                        <td><img src="{{ asset('storage/' .$users->images ) }}" alt="" width="30" height="30"></td>
+                        <td>{{ $post->user->username }}</td>
+                        <td>{{ $post->post}}</td>
+                        <td>{{ $post->created_at }}</td>
+                        <td>{{ $post->updated_at }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+        </div>
     </div>
 </div>
 @endsection
